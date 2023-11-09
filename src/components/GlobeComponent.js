0b9398c5445fd5fa3399b0f0733f874a2498f13c
George Townley <george@knowlesavenue.com>
2023-11-09 10:36:44 +0000

    add arc functionality

--- src/components/GlobeComponent.js
+++ src/components/GlobeComponent.js
@@ -12,8 +12,19 @@ function GlobeComponent() {
     },
   ]);
 
+  // Define the arcs data
+  const [arcsData] = useState([
+    {
+      startLat: 51.5074, // London latitude
+      startLng: -0.1278, // London longitude
+      endLat: 48.8566, // Paris latitude
+      endLng: 2.3522, // Paris longitude
+      color: "lightblue",
+    },
+  ]);
+
   return (
-    <div id="Globe-container" style={{ width: "100%", height: "600px" }}>
+    <div id="Globe-container" style={{ width: "100%", height: "100%" }}>
       <Globe
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
         bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
@@ -28,8 +39,15 @@ function GlobeComponent() {
         labelResolution={3}
         labelIncludeDot={true}
         labelDotRadius={0.1}
-        labelDotOrientation="bottom" // Changed to a string instead of a function that returns a string
+        labelDotOrientation="bottom"
         labelsTransitionDuration={1000}
+        // Add arcs related properties
+        arcsData={arcsData} // pass arcs data to the globe
+        arcColor="color"
+        arcAltitudeAutoScale={0.1}
+        arcStroke={0.5}
+        arcDashLength={0.3} // this will make the arc dashed
+        arcDashGap={0.1} // this will determine the gap between dashes
       />
     </div>
   );
