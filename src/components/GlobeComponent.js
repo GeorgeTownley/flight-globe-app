c5250fa76df6d7b93cfbc6eea63d32a24901f391
George Townley <george@knowlesavenue.com>
2023-11-10 13:16:02 +0000

    add api test

--- src/components/GlobeComponent.js
+++ src/components/GlobeComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Globe from "react-globe.gl";
 
 function GlobeComponent() {
@@ -17,11 +17,38 @@ function GlobeComponent() {
     {
       startLat: 51.5074, // London latitude
       startLng: -0.1278, // London longitude
-      endLat: -34.3568,
-      endLng: 18.4735,
+      endLat: -34.3568, // Cape of Good Hope latitude
+      endLng: 18.4735, // Cape of Good Hope longitude
       color: "lightblue",
     },
   ]);
+
+  useEffect(() => {
+    // Fetch the data from the AviationStack API when the component mounts
+    const API_KEY = process.env.REACT_APP_API_KEY; // Make sure your .env file has REACT_APP_API_KEY defined.
+    const flightNumber = "DAL75"; // Replace with a real flight number for testing.
+
+    const apiUrl = `http://api.aviationstack.com/v1/flights?access_key=${API_KEY}&flight_iata=${flightNumber}`;
+
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            "Network response was not ok: " + response.statusText
+          );
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Flight data:", data); // Log the data to the console
+      })
+      .catch((error) => {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+      });
+  }, []);
 
   return (
     <div id="Globe-container" style={{ width: "100%", height: "100%" }}>
@@ -41,14 +68,13 @@ function GlobeComponent() {
         labelDotRadius={0.1}
         labelDotOrientation="bottom"
         labelsTransitionDuration={1000}
-        // Add arcs related properties
-        arcsData={arcsData} // pass arcs data to the globe
+        arcsData={arcsData}
         arcColor="color"
         arcAltitudeAutoScale={0.1}
         arcStroke={0.1}
         arcAltitude={0.3}
-        arcDashLength={1} // this will make the arc dashed
-        arcDashGap={0.1} // this will determine the gap between dashes
+        arcDashLength={1}
+        arcDashGap={0.1}
       />
     </div>
   );
